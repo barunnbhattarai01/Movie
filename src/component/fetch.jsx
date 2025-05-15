e930bbb2760dbc11a2fd10a1dd2d16d9aa9af289
barunnbhattarai01 <barunnbhattarai@gmail.com>
2025-05-15 17:42:34 +0545

    push api to .env

--- src/component/fetch.jsx
+++ src/component/fetch.jsx
@@ -5,8 +5,10 @@ import Card from "./card";
 
 
 function Fetch(){
-const API_URL= "https://api.themoviedb.org/3/movie/popular?api_key=ba8eda4761510d8b37fe161e4371b7f2&language=en-US&page=1";
-const Acess_Token="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYThlZGE0NzYxNTEwZDhiMzdmZTE2MWU0MzcxYjdmMiIsIm5iZiI6MTc0NzMwNTkxNy4zMjgsInN1YiI6IjY4MjVjNWJkNzFlMzAyM2ZmMWExNzRkZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MaKKN-N-hoR_6ocbaHhgsZlhgDhYGB4UR-JFfUplcEc";
+     const ApiKey = import.meta.env.VITE_TMDB_API_KEY;
+  const AccessToken = import.meta.env.VITE_TMDB_ACCESS_TOKEN;
+
+const API_URL= "https://api.themoviedb.org/3/movie/popular?api_key=${ApiKey}&language=en-US&page=1";
 
 const [movies,setMovies]=React.useState([]);
 
