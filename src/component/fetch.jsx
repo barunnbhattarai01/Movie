7b8d5b98c12b033d639f468c27c98d6c38fede77
barunnbhattarai01 <barunnbhattarai@gmail.com>
2025-05-16 17:10:52 +0545

    search bar functionality added

--- src/component/fetch.jsx
+++ src/component/fetch.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
 import Card from "./card";
+import Start from "./start";
 
 
 
@@ -8,9 +9,10 @@ function Fetch(){
      const ApiKey = import.meta.env.VITE_TMDB_API_KEY;
   const AccessToken = import.meta.env.VITE_TMDB_ACCESS_TOKEN;
 
-const API_URL= "https://api.themoviedb.org/3/movie/popular?api_key=${ApiKey}&language=en-US&page=1";
+const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${ApiKey}&language=en-US&page=1`;
 
 const [movies,setMovies]=React.useState([]);
+const[search,setSearch]=React.useState("");
 
 
 useEffect(()=>{
@@ -29,31 +31,50 @@ useEffect(()=>{
    .catch(err=>console.error('Error fetching movies:',err));
    // console.log(setMovies)
 
-},[])
+},[]);
+
+useEffect(()=>{
+  if(search)
+  fetch(`https://api.themoviedb.org/3/search/movie?query=${search}&api_key=${ApiKey}`)
+  .then((res)=>res.json())
+  .then((data)=> setMovies(data.results) )
+  .catch((err)=>console.error("Error",err))
+  
+}
+
+,[search,ApiKey])
 
 //for fetching movie trailer
-const fetchTrailer = async (movieId) => {
-  const res = await fetch(
-    `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${ApiKey}`
-  );
-  const data = await res.json();
-  const trailer = data.results.find(
-    (vid) => vid.type === "Trailer" && vid.site === "YouTube"
-  );
-  if (trailer) {
-    window.open(`https://www.youtube.com/watch?v=${trailer.key}`, "_blank");
-  } else {
-    alert("Trailer not available");
-  }
+
+
+
+
+const fetchTrailer =async(movieId)=>{
+  const res =await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${ApiKey}`);
+  const data=await res.json();
+ const trailer =data.results.find(
+  (vid)=>vid.type==="Trailer" && vid.site==="YouTube"
+ );
+ if(trailer){
+  window.open(`https://www.youtube.com/watch?v=${trailer.key}`, "_blank");
+ }
+else{
+  alert("trailer not avaiable");
+}
 };
+
 
 
 return(
 <div>
+  <Start
+    onSearch={setSearch}
+    />
     <Card
       movies={movies}
       fetchTrailer={fetchTrailer}
     />
+   
 </div>
 
 )
